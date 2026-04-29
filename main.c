e06bab541c7ba6abaf28ce267af754bec0e44bd3
Eqiu2022 <Eqiu2022@gmail.com>
2026-04-29 13:03:55 -0700

    modified main.c

--- main.c
+++ main.c
@@ -1,17 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(char *args[], int argc) {
+int main( int argc, char *args[]) {
 
     FILE* fptr;
     char filename[100];
+    
     printf("Enter the filename: ");
     scanf("%s", filename);
     
-    fptr = fopen("filename.txt", "r");
+    fptr = fopen(filename, "r");
 
     if (fptr == NULL) {
         printf("The file is not opened.");
+        return 1;
     }
+
+    fclose(fptr)
     return 0;
 }
