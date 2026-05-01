d61b2b7779ead3097340d74263a358ae4c700821
Eqiu2022 <Eqiu2022@gmail.com>
2026-04-30 17:16:30 -0700

    update main

--- main.c
+++ main.c
@@ -3,8 +3,10 @@
 #include <pthread.h>
 #include <string.h>
 #include <unistd.h>
+#include <semaphore.h>
 
 #define MAX_NAME_LEN 256
+#define MAX_FILES 64
 
 int create_file(const char *name);
 int open_file(const char *name);
@@ -16,6 +18,10 @@ struct File{
     int is_open;
     int size;
 };
+
+p_thread_mutex_t mutex_lock;
+struct File files[MAX_FILES];
+sem_t file_semaphore;
 
 int main( int argc, char *args[]) {
 
@@ -34,4 +40,19 @@ int main( int argc, char *args[]) {
 
     fclose(fptr);
     return 0;
+}
+
+int create_file(const char *name) {
+    pthread_mutex_lock(&mutex_lock);
+    for (int i = 0; i < MAX_FILES; i++) {
+        if (files[i].name[0] == '\0') {
+            strncpy(files[i].name, name, MAX_NAME_LEN);
+            files[i].is_open = 0;
+            files[i].size = 0;
+            pthread_mutex_unlock(&mutex_lock);
+            return 0; 
+        }
+    }
+    pthread_mutex_unlock(&mutex_lock);
+    return -1; 
 }
